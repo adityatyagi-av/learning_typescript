04f91b65180b64cc1005987b7145da9076fed43a
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-06-06 21:55:32 +0530

    Learned about basic types in js

---
+++ Lesson_02/build/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+let myName = 'Aditya Tyagi';
+let meaningOfLife;
+let isLoading;
+// type: 'any' is used to assign any data type 
+let album;
+myName = 'Aadi';
+myName = 'Vashu';
+meaningOfLife = 4222;
+isLoading = true;
+album = 1942;
+album = 'aadi';
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ Lesson_02/src/main.ts
@@ -0,0 +1,21 @@
+let myName: string = 'Aditya Tyagi'
+let meaningOfLife: number;
+let isLoading: boolean;
+// type: 'any' is used to assign any data type 
+let album: any;
+
+myName = 'Aadi'
+myName = 'Vashu'
+meaningOfLife =4222
+isLoading =true
+album=1942
+album='aadi'
+
+const sum =(a:number,b:string)=>{
+    return a+b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp =/\w+/g

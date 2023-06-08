3700c844e118d817301d0c71d47658e1a41d9a74
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-06-08 22:31:51 +0530

    type,interface and enums explored

--- Lesson_03/build/js/main.js
+++ Lesson_03/build/js/main.js
@@ -19,3 +19,55 @@ let myObj;
 myObj = [];
 console.log(typeof myObj);
 myObj = myTuple;
+myObj = {};
+const exampleObject = {
+    prop1: 'aditya',
+    prop2: true,
+};
+let evh = {
+    name: "Aadi",
+    albums: [2025, 'aadi', 'I'],
+    active: true,
+};
+let jb = {
+    name: 'Tyagi',
+    albums: [2006, 'Dawood'],
+};
+evh = jb;
+console.log(jb);
+console.log(typeof (jb));
+console.log(typeof (evh));
+const greetGuitarist = (guitarist) => {
+    return `Hello ${guitarist.name}!`;
+};
+console.log(greetGuitarist(evh));
+let hey1 = {
+    name: 'aditya',
+    age: 21,
+    male: true,
+    address: [12],
+};
+const greetBoy = (boytype) => {
+    var _a;
+    return `hello boy ${(_a = boytype.name) === null || _a === void 0 ? void 0 : _a.toLowerCase}`;
+};
+//Enums - Enums allow to define a set of named constants. Using enums can make it easier to document intent, or create a set of distinct cases.Typescript provides both numeric and string-based enums.
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "UP";
+    Direction["Down"] = "DOWN";
+    Direction["Left"] = "LEFT";
+    Direction["Right"] = "";
+})(Direction || (Direction = {}));
+var Value;
+(function (Value) {
+    Value[Value["Up"] = 0] = "Up";
+    Value[Value["Down"] = 2] = "Down";
+    Value[Value["Right"] = 3] = "Right";
+    Value[Value["None"] = 4] = "None";
+    Value[Value["Hey"] = 5] = "Hey";
+    Value[Value["You"] = 6] = "You";
+    Value[Value["Heyyy"] = 7] = "Heyyy";
+    Value[Value["Left"] = 6] = "Left";
+    Value[Value["Go"] = 7] = "Go";
+})(Value || (Value = {}));
--- Lesson_03/src/main.ts
+++ Lesson_03/src/main.ts
@@ -1,25 +1,16 @@
 //working with the arrays
 let stringArr =['one','two','hey']
-
 let guitars =['Strat','Les Paul',5150]
-
 let mixedData=[false,'evh',129,true]
-
 stringArr[0]='Aditya'
 stringArr.push('Tyagi')
 
-
 guitars[0]=12
 guitars.unshift('Strat')
-
  let test=[]
-
  let band: string[]=[]
-
  band.push('Tyagi')
-
 console.log(typeof(band))
-
 //Tuples
 let myTuple: [string,number,boolean] =['Aditya Tyagi',21,true]
 
@@ -32,5 +23,74 @@ console.log(typeof myObj)
 
 myObj =myTuple
 
+myObj={}
 
+const exampleObject ={
+    prop1: 'aditya',
+    prop2: true,
+}
+//to create any type we used the type
+type Guitarist = {
+    name: string,
+    active?: boolean,  // ? is to make this optional
+    albums: (string | number)[]
+}
+let evh: Guitarist ={
+    name: "Aadi",
+   
+    albums: [2025,'aadi','I'],
+    active: true,
+}
 
+let jb={
+    name: 'Tyagi',
+    albums: [2006,'Dawood'],
+    
+}
+evh =jb
+console.log(jb)
+console.log(typeof(jb));
+console.log(typeof(evh))
+const greetGuitarist =(guitarist: Guitarist)=>{ //parameters set here and passed
+    return `Hello ${guitarist.name}!`
+}
+console.log(greetGuitarist(evh))
+//Using Interface
+interface boy{
+    name?: string,
+    age: number,
+    male: boolean,
+}
+interface boy{
+    address: (string|number)[]
+}
+let hey1: boy={
+    name: 'aditya',
+    age: 21,
+    male: true,
+    address: [12],
+
+}
+const greetBoy=(boytype: boy)=>{
+   return `hello boy ${boytype.name?.toLowerCase}`
+}
+//Enums - Enums allow to define a set of named constants. Using enums can make it easier to document intent, or create a set of distinct cases.Typescript provides both numeric and string-based enums.
+
+enum Direction {
+    Up = "UP",
+    Down = "DOWN",
+    Left = "LEFT",
+    Right =""
+  }
+
+  enum Value{
+    Up,
+    Down=2,
+    Right,
+    None,
+    Hey,
+    You,
+    Heyyy,
+    Left=6,
+    Go
+  }

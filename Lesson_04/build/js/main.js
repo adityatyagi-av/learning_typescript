e937b5a5d0e02fe259bd3e5fa12d3754891ff26b
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-06-18 12:35:57 +0530

    learned about the functions

--- Lesson_04/build/js/main.js
+++ Lesson_04/build/js/main.js
@@ -1,2 +1,84 @@
 "use strict";
 //Type Aliases 
+// interface PostId =stringOrNumber
+//Literal Types
+let myName;
+let userName;
+userName = 'Aditya';
+//function 
+const add1 = (car) => {
+    var _a;
+    return (_a = car.name) === null || _a === void 0 ? void 0 : _a.toUpperCase;
+};
+const add = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg('hey buddy');
+logMsg(add(1, 2));
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()} !`);
+}
+greet('aditya', new Date());
+let subtract = function (c, d) {
+    return c - d;
+};
+let multiply = (c, d) => {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+logMsg(subtract(2, 6));
+//optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return b + c;
+    }
+    else
+        return a;
+};
+//default parameters
+const sumAll = (a = 11, b, c = 12) => {
+    return a + b + c;
+};
+logMsg(addAll(['sa', 12, 'aditya'], 12));
+logMsg(sumAll(2, 3));
+logMsg(sumAll(undefined, 12));
+//Rest Parameters
+const total = (a, ...num) => {
+    return a + num.reduce((prev, curr) => prev - curr);
+};
+logMsg(total(2, 12, 1, 12, 42));
+//never type explored
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+//custom guard
+const isNumber = (value) => {
+    return typeof value === 'number'
+        ? true : false;
+};
+const isString = (value) => {
+    return typeof value === 'string'
+        ? true : false;
+};
+//use of the never type
+const numberOrString = (value) => {
+    if (isString(value))
+        return 'string';
+    if (isNumber(value))
+        return 'number';
+    return createError('error never occured');
+};
--- Lesson_04/src/main.ts
+++ Lesson_04/src/main.ts
@@ -20,3 +20,102 @@ let myName: 'Aditya'
 let userName: 'Aditya' | 'Tyagi' | 'aadi'
 
 userName='Aditya'
+
+//function 
+
+const add1=(car: car)=>{
+    return car.name?.toUpperCase
+}
+const add =(a:number, b:number, c?:number):
+ number=>{
+    if(typeof c !== "undefined"){
+        return a+b+c;
+    }
+    return a+b
+}
+
+
+const logMsg =(message: any): void=>{
+    console.log(message)
+}
+logMsg('hey buddy')
+logMsg(add(1,2))
+
+function greet(person:string, date: Date){
+    console.log(`Hello ${person}, today is ${date.toDateString()} !`)
+}
+greet('aditya',new Date());
+
+let subtract = function (c:number,d:number):
+number{
+    return c-d
+}
+type mathFunction =(a:number,b:number)=> number
+
+let multiply: mathFunction =(c,d)=>{
+    return c*d
+}
+
+logMsg(multiply(2,2))
+logMsg(subtract(2,6))
+
+//optional parameters
+
+const addAll=(a:(string|number)[] , b:number , c?:number)=>{
+    if(typeof c !== 'undefined'){
+        return b+c
+    }
+    else
+    return a
+}
+
+
+//default parameters
+const sumAll =(a:number=11,b:number,c:number =12)
+:number=>{
+    return a+b+c
+}
+
+logMsg(addAll(['sa',12,'aditya'],12))
+logMsg(sumAll(2,3))
+logMsg(sumAll(undefined,12))
+
+//Rest Parameters
+const total =(a:number,...num: number[]):number=>{
+    return a+num.reduce((prev,curr)=>prev-curr)
+}
+
+logMsg(total(2,12,1,12,42))
+
+
+//never type explored
+const createError =(errMsg: string):never=>{
+    throw new Error(errMsg)
+}
+
+const infinite =()=>{
+    let i:number=1
+    while(true){
+        i++
+        if(i>100) break
+    }
+}
+
+//custom guard
+const isNumber=(value:number|string):boolean=>{
+    return typeof value ==='number'
+    ? true: false
+}
+const isString =(value:any):boolean=>{
+    return typeof value==='string'
+    ? true: false
+}
+
+
+//use of the never type
+const numberOrString =(value:number|string):string=>{
+    if(isString(value)) return 'string'
+    if(isNumber(value)) return 'number'
+
+    return createError('error never occured')
+}

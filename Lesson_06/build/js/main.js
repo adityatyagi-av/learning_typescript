5a4ac406e51ca077a058003948554edce65bda27
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-06-18 23:22:55 +0530

    classes explored in ts

--- Lesson_06/build/js/main.js
+++ Lesson_06/build/js/main.js
@@ -1,33 +1,56 @@
 "use strict";
-//Classes in JAVASCRIPT
-class Coder {
-    constructor(name, music, age, lang = 'Typescript') {
+// //Classes in JAVASCRIPT
+class Guitarist {
+    constructor(name, instrument) {
         this.name = name;
-        this.music = music;
-        this.age = age;
-        this.lang = lang;
-        this.name = name;
-        this.music = music;
-        this.age = age;
-        this.lang = lang;
+        this.instrument = instrument;
     }
-    getAge() {
-        return `Hello, I'm ${this.age}`;
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
     }
 }
-const Aditya = new Coder('Aditya', 'Hip-Hop', 20, 'typescript');
-Aditya.music = 'melody';
-console.log(Aditya.getAge());
-// console.log(Aditya.age)
-class WebDev extends Coder {
-    constructor(computer, name, music, age) {
-        super(name, music, age); //super must be provided before this 
-        this.computer = computer;
-        this.computer = computer;
+const Page = new Guitarist('Aditya', 'violin');
+console.log(Page.play('plays'));
+///////////////////////////////////
+//static - Static properties cannot be directly accessed on instances of the class. Instead, they're accessed on the class itself.
+//
+class Peeps {
+    static getCount() {
+        return Peeps.count;
     }
-    getLang() {
-        return `I write ${this.lang}`;
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
     }
 }
-const Vashu = new WebDev('hp', 'Vashu', 'Lofi', 19);
-console.log(Vashu.getLang(), Vashu.getAge());
+Peeps.count = 0;
+const Tyagi = new Peeps('Aditya');
+const Tyagi1 = new Peeps('Aditya');
+const Tyagi2 = new Peeps('Aditya');
+console.log(Tyagi.id);
+console.log(Peeps.getCount());
+////////////////////////////////////
+//getter and setter in TypeScript
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error('Param is not an  array of strings');
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Adityaa', 'Tyagi'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'ZZ Top'];
+console.log(MyBands.data);
+MyBands.data = ['Van Halen'];
--- Lesson_06/src/main.ts
+++ Lesson_06/src/main.ts
@@ -1,44 +1,128 @@
-//Classes in JAVASCRIPT
+// //Classes in JAVASCRIPT
 
-class Coder{
-    secondLang!: string
+// class Coder{
+//     secondLang!: string
     
-    constructor(
-        public readonly name:string,
-        public music:string,
-        private age:number,
-        protected lang:string='Typescript'
-        )  {
-        this.name= name
-        this.music=music
-        this.age=age
-        this.lang=lang
+//     constructor(
+//         public readonly name:string,
+//         public music:string,
+//         private age:number,
+//         protected lang:string='Typescript'
+//         )  {
+//         this.name= name
+//         this.music=music
+//         this.age=age
+//         this.lang=lang
+//     }
+//     public getAge(){
+//         return `Hello, I'm ${this.age}`
+//     }
+// }
+
+// const Aditya =new Coder('Aditya','Hip-Hop',20,'typescript')
+// Aditya.music ='melody'
+// console.log(Aditya.getAge())
+// // console.log(Aditya.age)
+
+
+// class WebDev extends Coder {
+//     constructor(
+//         public computer: string,
+//         name:string,
+//         music:string,
+//         age:number
+//         ){
+//         super(name,music,age) //super must be provided before this 
+//         this.computer=computer
+//     }
+
+//     public getLang(){
+//         return `I write ${this.lang}`
+//     }
+// }
+// const Vashu =new WebDev('hp','Vashu','Lofi',19)
+// console.log(Vashu.getLang(),Vashu.getAge())
+
+
+
+
+////////////////////////////////////////////
+
+
+interface Musician{
+    name:string,
+    instrument: string,
+    play(action: string): string
+}
+
+class Guitarist implements Musician{
+    name:string
+    instrument: string
+    constructor(name: string,
+        instrument: string){
+            this.name=name
+            this.instrument=instrument
+        }
+
+    play(action:string){
+        return `${this.name} ${action} the ${this.instrument}`
     }
-    public getAge(){
-        return `Hello, I'm ${this.age}`
+}
+const Page = new Guitarist('Aditya','violin')
+console.log(Page.play('plays'))
+
+
+///////////////////////////////////
+
+//static - Static properties cannot be directly accessed on instances of the class. Instead, they're accessed on the class itself.
+//
+class Peeps{
+    static count:number =0
+
+    static getCount():number{
+        return Peeps.count
+    }
+
+    public id:number
+
+    constructor(public name:string){
+        this.name=name
+        this.id=++Peeps.count
+    }
+}
+const Tyagi =new Peeps('Aditya')
+const Tyagi1 =new Peeps('Aditya')
+const Tyagi2=new Peeps('Aditya')
+
+
+console.log(Tyagi.id)
+console.log(Peeps.getCount())
+
+
+////////////////////////////////////
+
+
+//getter and setter in TypeScript
+class Bands{
+    private dataState: string[]
+
+    constructor(){
+        this.dataState=[]
+    }
+    public get data(): string[]{
+        return this.dataState
+    }
+    public set data (value: string[]) {
+        if (Array.isArray (value) && value.every(el => typeof el === 'string')){
+        this.dataState = value
+        return
+        } else throw new Error('Param is not an  array of strings')
     }
 }
 
-const Aditya =new Coder('Aditya','Hip-Hop',20,'typescript')
-Aditya.music ='melody'
-console.log(Aditya.getAge())
-// console.log(Aditya.age)
-
-
-class WebDev extends Coder {
-    constructor(
-        public computer: string,
-        name:string,
-        music:string,
-        age:number
-        ){
-        super(name,music,age) //super must be provided before this 
-        this.computer=computer
-    }
-
-    public getLang(){
-        return `I write ${this.lang}`
-    }
-}
-const Vashu =new WebDev('hp','Vashu','Lofi',19)
-console.log(Vashu.getLang(),Vashu.getAge())
+const MyBands =new Bands()
+MyBands.data=['Adityaa','Tyagi']
+console.log(MyBands.data)
+MyBands.data=[...MyBands.data,'ZZ Top']
+console.log(MyBands.data)
+MyBands.data=['Van Halen']
